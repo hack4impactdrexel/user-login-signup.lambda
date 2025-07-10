90924215477f38c5909a4c794cd0dc3f26a518da
prometheus6011 <mahidhar1287@gmail.com>
2025-07-10 18:12:48 -0400

    Initial Commit

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ from typing import Dict, Any
 import boto3
 
 def handler(event: events.APIGatewayProxyEventV2, context: context_.Context) -> Dict[str, Any]:
-	print(event)
-	print(context)
-
-	return {
+    print(event)
+    print(context)
+	
+    return {
         'statusCode': 200,
         'body': 'Hello from Lambda!'
     }
